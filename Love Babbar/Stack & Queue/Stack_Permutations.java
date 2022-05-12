b0c4b91bc609647906d2c089c7fd9ac6d26de0f9
Sanjay Bisht <sanjaybishtamazon@gmail.com>
2022-05-13 03:28:52 +0530

    Update Stack_Permutations.java
    Correct Code

--- Love Babbar/Stack & Queue/Stack_Permutations.java
+++ Love Babbar/Stack & Queue/Stack_Permutations.java
@@ -1,8 +1,21 @@
 /**
  * Stack_Permutations
  */
+import java.util.*;
 public class Stack_Permutations {
     public static void main(String[] args) {
-        
+    int n=3;
+    int A[]= {2,4,3,1};
+    int B[]= {3,4,1,2};
+    int y=0;
+    Stack <Integer> S= new Stack<>();
+    for (int i = 0; i < A.length; i++) {
+        S.push(A[i]);
+        while(!S.isEmpty() && S.peek()==B[y]){
+            S.pop();
+            y++;
+        }
+    }
+    
     }
 }
