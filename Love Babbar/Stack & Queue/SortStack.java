29ee3f10101f6bfdd14e2633cfff21b783b8a6bd
Sanjay Bisht <sanjaybishtamazon@gmail.com>
2022-05-16 04:15:18 +0530

    Completed Sorted Stack

--- Love Babbar/Stack & Queue/SortStack.java
+++ Love Babbar/Stack & Queue/SortStack.java
@@ -3,6 +3,7 @@
  */
 import java.util.*;
 public class SortStack {
+    static Stack<Integer> sorted;
     public static void main(String[] args) {
         Stack<Integer> s= new Stack<>();
         s.push(11);
@@ -10,12 +11,30 @@ public class SortStack {
         s.push(32);
         s.push(3);
         s.push(41);
-        Stack<Integer> sorted= sort(s);
+        sort(s);
         System.out.println(s);
-        System.out.println(sorted);
     }
     public static Stack<Integer> sort(Stack<Integer> s)
 	{
-		return s;
+        sorted=s;
+        sortStack();
+		return sorted;
 	}
+    static void sortStack(){
+        if(sorted.isEmpty()){
+            return;
+        }
+        int temp=sorted.pop();
+        sortStack();
+        insertsort(temp);
+    }
+    static void insertsort(int temp){
+        if(sorted.isEmpty() || sorted.peek()<temp){
+            sorted.add(temp);
+            return;
+        }
+        int x=sorted.pop();
+        insertsort(temp);
+        sorted.add(x);
+    }
 }
