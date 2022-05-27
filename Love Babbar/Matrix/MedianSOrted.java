bc5507ca86aad73dcea7730b9a23cfed8e5233e2
Sanjay Bisht <sanjaybishtamazon@gmail.com>
2022-05-27 11:21:56 +0530

    Update Median Sort

--- Love Babbar/Matrix/MedianSOrted.java
+++ Love Babbar/Matrix/MedianSOrted.java
@@ -11,14 +11,33 @@ public class MedianSOrted {
             {2,6,9},
             {3,6,9}
         };
+        int n=R*C;
         int left=0;
         int right=2000;
         while(left<=right){
-            int mid=left+(right-left)/2;
-            int x=checkforthis(mid);
-            if(x==1){
-                break;
+            int mid=(right+left)/2;
+            int ans=0;
+            for (int i = 0; i < R; i++) {
+                int l=0;
+                int h=C-1;
+                while(l<=h){
+                    int m=l+(h-l)/2;
+                    if(M[i][m]<=mid){
+                        l=m+1;
+                    }
+                    else{
+                        h=m-1;
+                    }
+                }
+                ans=ans+l;
+            }
+            if(ans<=n/2){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
             }
         }
+        System.out.println(left);
     }
 }
